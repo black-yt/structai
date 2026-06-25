d20df602578bee124a9c93b293493e63212d0aab
black-yt <2667685528@qq.com>
2026-06-25 10:18:06 +0800

    Bump version to 0.1.24

--- tests/test_public_api.py
+++ tests/test_public_api.py
@@ -5,7 +5,7 @@ import structai
 
 class PublicApiTests(unittest.TestCase):
     def test_public_package_import_and_version(self):
-        self.assertEqual(structai.__version__, "0.1.23")
+        self.assertEqual(structai.__version__, "0.1.24")
 
     def test_core_symbols_are_exported(self):
         for name in (
